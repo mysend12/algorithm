e97d87bc40268751f3866264d88bb12cadaf0435
mysend <mysend12@naver.com>
2022-09-27 12:18:40 +0900

    queue에 저장할때, 불필요한 조건 삭제

--- src/io/my/programmers/Solution1844.java
+++ src/io/my/programmers/Solution1844.java
@@ -16,8 +16,8 @@ public class Solution1844 {
     }
 
     public static int solution(int[][] maps) {
-        int lastX = maps[0].length - 1;
-        int lastY = maps.length - 1;
+        int width = maps[0].length - 1;
+        int height = maps.length - 1;
 
         Queue<Coordinate> queue = new LinkedList<>();
         queue.add(new Coordinate(0, 0, 1));
@@ -37,12 +37,12 @@ public class Solution1844 {
             if (y-1 >= 0 && maps[y-1][x] == 1) {
                 addQueue(queue, passMap, x, y-1, index);
             }
-            if (lastX >= x+1 && maps[y][x+1] == 1) {
-                if (x+1 == lastX && y == lastY) return index;
+            if (width >= x+1 && maps[y][x+1] == 1) {
+                if (x+1 == width && y == height) return index;
                 addQueue(queue, passMap, x+1, y, index);
             }
-            if (lastY >= y+1 && maps[y+1][x] == 1) {
-                if (x == lastX && y+1 == lastY) return index;
+            if (height >= y+1 && maps[y+1][x] == 1) {
+                if (x == width && y+1 == height) return index;
                 addQueue(queue, passMap, x, y+1, index);
             }
         } while(!queue.isEmpty());
@@ -53,46 +53,8 @@ public class Solution1844 {
     static void addQueue(Queue<Coordinate> queue, Map<Coordinate, Byte> passMap, int x, int y, int index) {
         Coordinate coordinate = new Coordinate(x, y, index);
         if (passMap.containsKey(coordinate)) return;
-        if (queue.isEmpty() || queue.stream().noneMatch(e -> e.equals(coordinate))) queue.add(coordinate);
+        queue.add(coordinate);
     }
-
-//    static class Coordinate {
-//        final int x;
-//        final int y;
-//        final int index;
-//
-//        public Coordinate(int x, int y, int index) {
-//            this.x = x;
-//            this.y = y;
-//            this.index = index;
-//        }
-//
-//        public int x() {
-//            return x;
-//        }
-//
-//        public int y() {
-//            return y;
-//        }
-//
-//        public int index() {
-//            return index;
-//        }
-//
-//        @Override
-//        public boolean equals(Object o) {
-//            if (this == o) return true;
-//            if (o == null || getClass() != o.getClass()) return false;
-//            Coordinate that = (Coordinate) o;
-//            return x == that.x && y == that.y;
-//        }
-//
-//        @Override
-//        public int hashCode() {
-//            return Objects.hash(x, y);
-//        }
-//    }
-
 }
 
 record Coordinate(int x, int y, int index) {

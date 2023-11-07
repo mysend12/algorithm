90ba9011dff232ec9ab163805810a0b15504fcb4
mysend12 <mysend12@naver.com>
2023-11-07 21:48:03 +0900

    프로그래머스 - 콜라츠 추측
    https://school.programmers.co.kr/learn/courses/30/lessons/12943

---
+++ etc/콜라츠_추측.kt
@@ -0,0 +1,15 @@
+package etc
+
+class 콜라츠_추측 {
+    fun solution(num: Int): Int {
+        if (num == 1) return 0
+        return recursion(num.toLong(), 0)
+    }
+
+    private fun recursion(num: Long, count: Int): Int = when {
+        num == 1L -> count
+        count >= 500 -> -1
+        num % 2 == 0L -> recursion(num / 2, count + 1)
+        else -> recursion(num * 3 + 1, count + 1)
+    }
+}
